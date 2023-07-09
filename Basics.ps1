4d6f47f230e132ea61097741c273b8cefa5204f5
Lee <lee.cook@live.com.au>
2023-07-10 07:30:25 +1000

    Update Basics.ps1

--- Basics.ps1
+++ Basics.ps1
@@ -1,5 +1,24 @@
+#List loaded modules on your local Powershell install
+Get-Module
+
+#Connect to an Azure subscription
+Connect-AzAccount
+
+#List all Resource Groups in the Azure subscription
+Get-AzResourceGroup
+
+#Display all Azure Resource Groups in table format
+Get-AzResourceGroup | Format-Table
+
+#Filter
+
+#Create a new Azure Resource group
+New-AzResourceGroup -Name <name> -Location <location>
+
+
+
+#Create new VM in an Azure subscription
 New-AzVm `
-#
     -ResourceGroupName "CrmTestingResourceGroup" `
     -Name "CrmUnitTests" `
     -Image "UbuntuLTS"
